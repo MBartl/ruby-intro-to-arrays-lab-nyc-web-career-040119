b909f21d9a09fa2d8882f3e0fefe111d159ec9e4
Matthew Bartl <mattbartl@gmail.com>
2019-03-19 18:36:41 +0000

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,32 @@
+def instantiate_new_array 
+  []
+end
+
+def array_with_two_elements
+  patterns = ['argyle', 'chevron']
+  return patterns
+end
+
+def first_element(brody)
+  brody[0]
+end
+
+def third_element(nomorelawjobs)
+  nomorelawjobs[2]
+end
+
+def last_element(flatironIsAwesome)
+  flatironIsAwesome[-1]
+end
+
+def first_element_with_array_methods(deskjobs)
+  firstelement = deskjobs.shift()
+end
+
+def last_element_with_array_methods(cats)
+  lastelement = cats.pop()
+end
+
+def length_of_array(silly)
+  silly.length
+end
